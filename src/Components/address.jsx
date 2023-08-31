5063d4bd022a816a12ff8063715f053bffdbd7dc
kaigwa <kaigwaedwin@gmail.com>
2023-08-31 12:09:14 +0300

    First commit

---
+++ src/Components/address.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+function Address() {
+    const [posts, usePosts] = useState([]);
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default Address;
